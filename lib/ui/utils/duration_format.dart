f8a39ba0d8a2dd3c0be95e67c5d6385f091037d2
Sotheara Kon <sothearakon73@gmail.com>
2026-03-25 17:28:42 +0700

    update duration format

--- lib/ui/utils/duration_format.dart
+++ lib/ui/utils/duration_format.dart
@@ -1,8 +1,8 @@
-// ignore: non_constant_identifier_names
 String DurationFormat(Duration duration) {
-  String twoDigits(int n) => n.toString().padLeft(1, '0');
+  String twoDigits(int n) => n.toString().padLeft(2, '0');
 
   final minutes = twoDigits(duration.inMinutes.remainder(60));
+  final seconds = twoDigits(duration.inSeconds.remainder(60));
 
-  return "$minutes mns";
+  return "$minutes:$seconds mns";
 }
